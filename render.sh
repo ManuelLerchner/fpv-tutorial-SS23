8007bb65385c821391d226a1e5749eba3d011e6d
ManuelLerchner <manuel.lerchner1111@gmail.com>
2023-05-06 11:19:05 +0200

    change scripts

--- render.sh
+++ render.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p docs
-pandoc -V documentclass=report -V classoption=oneside -V geometry:vmargin=14.5mm -V lang:de --fail-if-warnings --output=docs/summary.pdf summaries/*.md
+pandoc -V documentclass=report -V classoption=oneside -V geometry:vmargin=14.5mm -V lang:de --fail-if-warnings --output=docs/summary.pdf material/**/*.md
 
 # merge slides in material folder
 pdftk material/**/w*.pdf cat output docs/slides.pdf
