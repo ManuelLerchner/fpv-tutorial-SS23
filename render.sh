b3cf61af7810c126cfa5e60cb19ce304c497fcd5
ManuelLerchner <manuel.lerchner1111@gmail.com>
2023-05-06 02:47:30 +0200

    change folder structure
    Co-authored-by: Honas <ge65gom@mytum.de>

--- render.sh
+++ render.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p docs
-pandoc -V documentclass=report -V classoption=oneside -V geometry:vmargin=14.5mm -V lang:de --fail-if-warnings --output=docs/summary.pdf md/*.md
+pandoc -V documentclass=report -V classoption=oneside -V geometry:vmargin=14.5mm -V lang:de --fail-if-warnings --output=docs/summary.pdf summaries/*.md
 
-# merge slides in slide folder
-pdftk slides/*.pdf cat output docs/slides.pdf
+# merge slides in material folder
+pdftk material/**/w*.pdf cat output docs/slides.pdf
